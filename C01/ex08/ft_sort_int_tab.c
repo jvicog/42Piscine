89553a23f2992c79c140e685ada297ec2c2d3c16
jvicog <javiervico8@gmail.com>
2021-09-25 14:25:13 +0200

    C01

---
+++ C01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jvico-ga <jvico-ga@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/27 16:34:13 by jvico-ga          #+#    #+#             */
+/*   Updated: 2021/06/27 17:42:50 by jvico-ga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	aux;
+	int	boolean;
+	int	i;
+
+	boolean = 1;
+	while (boolean == 1)
+	{
+		boolean = 0;
+		i = 0;
+		while (i < size - 1)
+		{
+			if (tab[i] > tab[i + 1])
+			{
+				aux = tab[i];
+				tab[i] = tab[i + 1];
+				tab[i + 1] = aux;
+				boolean = 1;
+			}
+			i++;
+		}
+	}
+}
